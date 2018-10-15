7db1f8f5d17cc6791d754e3e97692f0a54bf5c82
Rafael Ferreira <rafael.ferreira@olxbr.com>
2018-10-15 18:05:43 -0300

    Added -t flag to kill env faster

--- scripts/resetenv.sh
+++ scripts/resetenv.sh
@@ -2,14 +2,13 @@
 #Removing containers, network and volumes
 echo
 echo "Stoping and removing containers, network and volumes"
-docker-compose down -v
+docker-compose down -v -t0
 
 #Removing working folders
 echo
 echo "Removing working folders.."
-rm -rf ../database/db
 rm -rf ../app/node_modules
-mkdir ../database/db
+rm -rf ../app/package-lock.json
 
 echo
 echo "Done"
